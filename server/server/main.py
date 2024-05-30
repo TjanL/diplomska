0f545c203be3e13ca52fa22866cfdd584135a0a5
TjanL <tljubesek@gmail.com>
2024-05-30 17:44:18 +0200

    added docker containers

--- server/server/main.py
+++ server/server/main.py
@@ -1,4 +1,6 @@
+import os
 from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session, select
@@ -10,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[os.environ.get("FRONTEND_HOST", "http://localhost:5173")],
 )
 
 
--- server/server/model.py
+++ server/server/model.py
@@ -1,3 +1,4 @@
+import os
 from typing import Optional
 
 from sqlmodel import Field, SQLModel, create_engine
@@ -7,12 +8,11 @@ class Counter(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     count: int
 
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
+print(os.environ.get("DATABASE_HOST", "sqlite:///database.db"))
 connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+engine = create_engine(
+    os.environ.get("DATABASE_HOST", "sqlite:///database.db"), connect_args=connect_args
+)
 
 
 def create_database_and_tables():
