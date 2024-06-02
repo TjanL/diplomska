2dc1c2f903f2fe911667b1b6559f175282f9a44d
TjanL <tljubesek@gmail.com>
2024-06-02 14:44:02 +0200

    added reslov.conf edit option

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+if [ -z "$INPUT_DNS_SERVERS" ]; then
+    echo -e $INPUT_DNS_SERVERS >> /etc/resolv.conf
+
 mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-cat ~/.ssh/id_rsa
 
 docker context create prod --docker host="ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST"
 docker context use prod
