2fa884834fc34b3023fe455cde983e5c99d17dbf
TjanL <tljubesek@gmail.com>
2024-06-02 13:37:14 +0200

    fixed action and added secrets

--- main.tf
+++ main.tf
@@ -7,11 +7,57 @@ terraform {
   }
 }
 
-provider "github" {}
+locals {
+  workflows = [
+    "monorepo-feature-docker",
+    "monorepo-trunk-docker",
+    "per-component-app-feature-docker",
+    "per-component-app-trunk-docker",
+    "per-component-server-feature-dockerr",
+    "per-component-server-trunk-docker",
+  ]
+}
 
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
+variable "ts_oauth_client_id" {}
+variable "ts_oauth_secret" {}
+variable "ssh_private_key" {}
 
+resource "github_repository" "repositories" {
+  for_each = toset(local.workflows)
+
+  name       = each.value
   visibility = "public"
+  auto_init  = true
+}
+
+resource "github_actions_secret" "tailscale_client_id" {
+  for_each = github_repository.repositories
+
+  repository      = each.value.name
+  secret_name     = "TS_OAUTH_CLIENT_ID"
+  plaintext_value = var.ts_oauth_client_id
+}
+
+resource "github_actions_secret" "tailscale_oauth" {
+  for_each = github_repository.repositories
+
+  repository      = each.value.name
+  secret_name     = "TS_OAUTH_SECRET"
+  plaintext_value = var.ts_oauth_secret
+}
+
+resource "github_actions_secret" "ssh_private_key" {
+  for_each = github_repository.repositories
+
+  repository      = each.value.name
+  secret_name     = "SSH_PRIVATE_KEY"
+  plaintext_value = var.ssh_private_key
+}
+
+resource "null_resource" "clone" {
+  for_each = github_repository.repositories
+
+  provisioner "local-exec" {
+    command = "gh repo clone ${each.value.name} ${path.module}/workflows/${each.value.name}"
+  }
 }
