0ae7f77bc21368f61997609a70bf75ea486ca514
TjanL <tljubesek@gmail.com>
2024-06-02 14:04:04 +0200

    removed redundant env vars

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -l
 
 mkdir -p ~/.ssh
-echo "$SSH_PRIVATE_KEY" >> ~/.ssh/id_rsa
+echo "$INPUT_SSH_PRIVATE_KEY" >> ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-docker context create prod --docker host="ssh://$SSH_USER@$SSH_HOST"
+docker context create prod --docker host="ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST"
 docker context use prod
 
-docker stop $(docker ps --filter name=$CONTAINER_NAME -q)
-docker run $DOCKER_VOLUMES $DOCKER_PORTS $DOCKER_ENV --detached --rm --name $CONTAINER_NAME $CONTAINER_IMAGE
+docker stop $(docker ps --filter name=$INPUT_CONTAINER_NAME -q)
+docker run $INPUT_DOCKER_VOLUMES $INPUT_DOCKER_PORTS $INPUT_DOCKER_ENV --detached --rm --name $INPUT_CONTAINER_NAME $INPUT_CONTAINER_IMAGE
