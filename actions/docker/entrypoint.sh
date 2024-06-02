af6478197802ffdd2cea1dfc77edaf207d5643bc
TjanL <tljubesek@gmail.com>
2024-06-02 16:34:38 +0200

    fix

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -4,10 +4,7 @@ set -e
 mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-ssh-keyscan $INPUT_SSH_HOST > ~/.ssh/config
-chmod 400 ~/.ssh/config
-
-cat ~/.ssh/config
+ssh-keyscan $INPUT_SSH_HOST > ~/.ssh/known_hosts
 
 docker context create prod --docker host="ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST"
 docker context use prod
