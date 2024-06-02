de827c3ec2156dd247941efcc2c9ad1dbedd8332
TjanL <tljubesek@gmail.com>
2024-06-02 16:17:26 +0200

    fix

--- actions/docker/entrypoint.sh
+++ actions/docker/entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 mkdir -p ~/.ssh
 echo "$INPUT_SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
+echo -e "Host *\n    StrictHostKeyChecking no" > ~/.ssh/config
+chmod 600 ~/.ssh/config
 
 docker context create prod --docker host="ssh://$INPUT_SSH_USER@$INPUT_SSH_HOST"
 docker context use prod
 
 docker stop $(docker ps --filter name=$INPUT_CONTAINER_NAME -q)
-docker run $INPUT_DOCKER_VOLUMES $INPUT_DOCKER_PORTS $INPUT_DOCKER_ENV --detached --rm --name $INPUT_CONTAINER_NAME $INPUT_IMAGE
+docker run $INPUT_DOCKER_VOLUMES $INPUT_DOCKER_PORTS $INPUT_DOCKER_ENV -d --rm --name $INPUT_CONTAINER_NAME $INPUT_IMAGE
